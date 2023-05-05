533c8c2f1c430ab538ea921834781bc89a033c93
ilona <gogiashvili.ilona@gmail.com>
2023-05-05 10:19:48 -0400

    Added component folder with Home and NavBar, added video on home page

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import video from "../assets/Video.mp4"
+
+const Home = () => {
+    return (
+        <section className='home'>
+
+            <div className='overlay'></div>
+            <video className='video' src={video} muted autoPlay loop type="video/mp4"></video>
+
+        </section>
+    )
+}
+
+export default Home
