272cad12a88f141026cad50623e857430f71d02e
ilona <gogiashvili.ilona@gmail.com>
2023-05-07 21:08:30 -0400

    fetched countries used localhost:9292/countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,8 +3,20 @@ import CountryLink from './CountryLink'
 
 const Countries = () => {
 
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:9292/countries')
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setCountries(data)
+            })
+    }, [])
+    const countriesList = countries.map(c => <CountryLink key={c.id} country={c} />)
     return (
         <div>
+            <ul>{countriesList}</ul>
 
         </div>
     )
