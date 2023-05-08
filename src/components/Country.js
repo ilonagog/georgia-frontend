2ec1d04a4f263466af1203133c6d76ed0f309c18
ilona <gogiashvili.ilona@gmail.com>
2023-05-07 21:47:13 -0400

    fixed issue, added tour component

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,12 +1,29 @@
-import React from 'react'
 
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Tour from '../components/Tour'
 
 const Country = () => {
+    const [country, setCountry] = useState({
+        tours: []
+    })
+    const params = useParams()
 
+    useEffect(() => {
+        fetch(`http://localhost:9292/countries/${params.id}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setCountry(data)
+            })
+    }, [])
+
+    const tours = country.tours.map(t => <Tour key={t.id} tour={t} />)
 
     return (
         <div>
-
+            <h1>{country.name}</h1>
+            <h3>Tours: {tours}</h3>
         </div>
     )
 }
--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const Tour = () => {
+const Tour = ({ tour }) => {
     return (
         <ul className='cards'>
-
+            <li className='card'>
+                <h3>{tour.name}</h3>
+                <figure className="image">
+                    <img src={tour.image_url} alt={tour.image_url} />
+                </figure>
+                <h3>Price: ${tour.price}</h3>
+                <h3>Days:{tour.days}</h3>
+            </li>
         </ul>
 
     )
 }
 
 export default Tour
+
